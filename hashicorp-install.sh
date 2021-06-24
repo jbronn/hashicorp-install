3d6c357f8426942076d2daa3c44990eba5734dfd
Justin Bronn <jbronn@gmail.com>
2021-06-24 15:16:00 -0600

    Update to the latest versions.

--- hashicorp-install.sh
+++ hashicorp-install.sh
@@ -13,19 +13,19 @@ PACKAGE_NAME="${1:-}"
 PACKAGE_ARCH="${PACKAGE_ARCH:-amd64}"
 case "$PACKAGE_NAME" in
     consul)
-        PACKAGE_VERSION="${PACKAGE_VERSION:-1.9.5}"
+        PACKAGE_VERSION="${PACKAGE_VERSION:-1.10.0}"
         ;;
     consul-esm)
         PACKAGE_VERSION="${PACKAGE_VERSION:-0.5.0}"
         ;;
     packer)
-        PACKAGE_VERSION="${PACKAGE_VERSION:-1.6.6}"
+        PACKAGE_VERSION="${PACKAGE_VERSION:-1.7.3}"
         ;;
     terraform)
-        PACKAGE_VERSION="${PACKAGE_VERSION:-0.15.1}"
+        PACKAGE_VERSION="${PACKAGE_VERSION:-1.0.1}"
         ;;
     vault)
-        PACKAGE_VERSION="${PACKAGE_VERSION:-1.7.1}"
+        PACKAGE_VERSION="${PACKAGE_VERSION:-1.7.3}"
         ;;
     *)
         if [ -n "$PACKAGE_NAME" ]; then
